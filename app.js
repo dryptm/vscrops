d892e16b418fbe20a15df1ce215deef85a1abba6
dryptm <vinay9415756155@gmail.com>
2021-06-20 23:56:23 +0530

    via console

--- app.js
+++ app.js
@@ -22,6 +22,38 @@ app.get("/home",function(req,res)
   
 })
 
+app.get("/products",function(req,res)
+{
+  res.render("products",{})
+  
+})
+app.get("/contactus",function(req,res)
+{
+  res.render("contactus",{})
+  
+})
+app.get("/foundersnote",function(req,res)
+{
+  res.render("foundersnote",{})
+  
+})
+app.get("/ourstory",function(req,res)
+{
+  res.render("ourstory",{})
+  
+})
+app.get("/blog",function(req,res)
+{
+  res.render("blog",{})
+  
+})
+
+
+
+
+
+
+
 app.listen(process.env.PORT || 3000, (req, res) => {
     console.log("server started")
 })
