5584d827eb849c7a7b2abaf088134d7768995173
dryptm <vinay9415756155@gmail.com>
2021-07-15 17:45:31 +0530

    via console

--- models/products.js
+++ models/products.js
@@ -5,7 +5,8 @@ const productSchema = new mongoose.Schema({
     product_price : Number,
     product_image : String,
     product_discount : Number,
-    product_sku : String
+    product_sku : String,
+    product_description:String
 });
 const Product = new mongoose.model("Products",productSchema);
 module.exports = Product;
