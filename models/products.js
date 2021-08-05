e78f6fb481a7b73fac8fee8a9c68c252006f5a88
dryptm <vinay9415756155@gmail.com>
2021-08-05 18:25:53 +0530

    via console

--- models/products.js
+++ models/products.js
@@ -4,6 +4,7 @@ const productSchema = new mongoose.Schema({
     product_name : String,
     product_price : Number,
     product_image : String,
+    product_quantity:String,
     product_discount : Number,
     product_sku : String,
     product_description:String
